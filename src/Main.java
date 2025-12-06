7846fe08f1d07e42cae38495187a944b47b03657
J-Herika <138695574+J-Herika@users.noreply.github.com>
2025-12-06 19:14:20 +0300

    Even or odd number identitfier

--- src/Main.java
+++ src/Main.java
@@ -1,16 +1,14 @@
 
 void main() {
-    Random random = new Random();
-    int model = random.nextInt(4);
 
-    switch (model) {
-        case 3 ->
-            IO.print("it's was made in big 5");
-        case 2->
-            IO.print("it's was made in big 00");
-        case 1->
-            IO.print("it's was made in big 90");
-        default -> IO.print("this beast is a classic!");
+    NumberTeller(23);
+
+}
+
+public void NumberTeller(int num){
+    if (num % 2 == 0 ){
+        IO.println("It is Even.");
+    } else if (num % 2 != 0) {
+        IO.println("It is Odd.");
     }
-
 }
