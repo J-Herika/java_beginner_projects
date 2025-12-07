867b92013517c07d69d9ff4af309c6bab027432b
J-Herika <138695574+J-Herika@users.noreply.github.com>
2025-12-07 17:35:36 +0300

    learn basic error handling use try ctch

--- src/Main.java
+++ src/Main.java
@@ -1,19 +1,11 @@
 
 void main() {
-// Make a method that reverses a string without using built-in reverse functions.
-    String anime = "Jujutsu Kaisen";
-    IO.println("Original String: " + anime);
-    IO.println("Reversed String: " + reverseString(anime));
 
-}
-
-
-public String reverseString(String string){
-    StringBuilder reversedStr = new StringBuilder();
-
-    for(int i = string.length() - 1; i >= 0; i--){
-           reversedStr.append(string.charAt(i));
+    try {
+        IO.println(10 / 0);
+    }catch (ArithmeticException e){
+        IO.println("ERROR: "+ e);
     }
 
-    return reversedStr.toString();
+
 }
