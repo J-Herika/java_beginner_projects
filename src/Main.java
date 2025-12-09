9cffb084de6c37329bbb0b74d86c6ce7e11b30f7
J-Herika <138695574+J-Herika@users.noreply.github.com>
2025-12-09 15:27:30 +0300

    a simple user manager project with persistent storage using I/O with text file

--- src/Main.java
+++ src/Main.java
@@ -1,6 +1,16 @@
 
 void main() {
 
+    UserManager userManager = new UserManager();
 
+    IO.println("--------------------");
+    userManager.registerUser(101,"Yami");
+    userManager.registerUser(23,"asta");
+    userManager.registerUser(141,"noal");
+    IO.println("--------------------");
+
+    IO.println(userManager.getBannedUsers());
+    userManager.banUser(101);
+    IO.println(userManager.getBannedUsers());
 
 }
--- src/UserManager.java
+++ src/UserManager.java
@@ -1,14 +1,22 @@
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.io.FileWriter;
+import java.io.IOException;
 import java.util.ArrayList;
 import java.util.HashMap;
+import java.util.Scanner;
 
 public class UserManager {
-    private ArrayList<String> bannedNames = new ArrayList<>();
-    private HashMap<Integer, String> activeUsers = new HashMap<>();
+    private final ArrayList<String> bannedNames = new ArrayList<>();
+    private final HashMap<Integer, String> activeUsers = new HashMap<>();
 
     public UserManager() {
         // Pre-fill banned list
         bannedNames.add("admin");
         bannedNames.add("null");
+        bannedNames.add("root");
+
+        updateBannedUsers();
     }
 
     public void registerUser(int id, String name) {
@@ -29,7 +37,62 @@ public class UserManager {
         System.out.println("Success: Registered " + name);
     }
 
+    public void updateBannedUsers(){
+        bannedNames.clear();
+        File file = new File("banned.txt");
+        try(Scanner reader = new Scanner(file)){
+
+            while(reader.hasNextLine()){
+                String data = reader.nextLine();
+                bannedNames.add(data);
+            }
+
+        } catch (FileNotFoundException e) {
+            IO.println("ERROR: couldn't find file.\n" + e);
+        }
+    }
+
+    public void banUser(int id){
+        if(activeUsers.containsKey(id)){
+            try(FileWriter fileWriter = new FileWriter("banned.txt",true)){
+                fileWriter.write(activeUsers.get(id)+"\n");
+                IO.println(activeUsers.get(id) + " is banned.");
+            } catch (IOException e) {
+                IO.println("Error: couldn't read file \n" + e);
+            }
+        }
+        updateBannedUsers();
+    }
+
+    public ArrayList getBannedUsers(){
+        return bannedNames;
+    }
+
+    public void removeBannedUsers(){
+        ArrayList<Integer> tempList = new ArrayList<>();
+        ArrayList<String> tempListNames = new ArrayList<>();
+        for(int id : activeUsers.keySet()){
+            if(bannedNames.contains(activeUsers.get(id))) {
+                tempList.add(id);
+                tempListNames.add(activeUsers.get(id));
+            }
+        }
+        IO.print("Removed these banned users: ");
+        for(String name : tempListNames){
+            IO.print(" " + name + ",");
+        }
+
+        for(int id : tempList){
+            activeUsers.remove(id);
+        }
+    }
+
+
+
+
     public void getUser(int id) {
         // Your logic here...
+        if(activeUsers.containsKey(id)) IO.println(activeUsers.get(id));
+        else IO.print("Error User is not registered.");
     }
 }
